177afaabbd62e4e5b89c123a9ac058db4bfc7099
tigreryder <tigreryder@gmail.com>
2017-02-02 15:17:28 -0800

    Add files via upload

--- create_text_feature.py
+++ create_text_feature.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import sys
+import csv
 import pickle
 from nltk.stem.snowball import SnowballStemmer
 from sklearn import tree
@@ -18,11 +19,10 @@ def parseOutText(f):
     example use case:
     f = open("email_file_name.txt", "r")
     text = parseOutText(f)
-        
+       
     '''
 
-
-    f.seek(0)  ### go back to beginning of file (annoying)
+    f.seek(0)  ### go back to beginning of file
     all_text = f.read()
 
     ### split off metadata
@@ -83,34 +83,24 @@ def predict_emails(email, word_data, author_data, vectorizer, classifier):
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-
-# find email addresses of persons of interest
-
+### Create empty lists to store email addresses for pois and non-pois
 poiEmails = []
+notPoiEmails = []
 
+### Retrieve email addresses from data_dict
 for person in data_dict:
     if data_dict[person]['poi'] == True:
         poiEmails.append(data_dict[person]['email_address'])
-
-
-# find email addresses of non persons of interest
-
-notPoiEmails = []
 
 for person in data_dict:
     if data_dict[person]['poi'] == False:
         if data_dict[person]['email_address'] != 'NaN':
             notPoiEmails.append(data_dict[person]['email_address'])
 
-
-# using email address data, navigate to file containing email paths by email address
-# and store to lists
-
+### Create lists to store file locations of emails
 poiEmailPaths = []
 notPoiEmailPaths = []
 author_data = []
-
-#using email paths, find emails and store text for both persons of interest and non persons of interest
 
 for email in poiEmails:
     path = 'emails_by_address/from_' + email + '.txt'
@@ -126,6 +116,7 @@ for email in poiEmails:
     except IOError:
         continue
     
+    
 for email in notPoiEmails:
     path = 'emails_by_address/from_' + email + '.txt'
     try:
@@ -140,11 +131,13 @@ for email in notPoiEmails:
     except IOError:
         continue
     
-# use function parseOutText() on emails to stem words and then store to word_data
-# store poi status to poi_data
 
+### prepare to extract text, stem words using SnowballStemmer, and store in word_data
 poi_data = []
 word_data = []
+
+### remove words in replace_list from word_data due to overvalued signifiance of the data by tfidf
+replace_list = ['ddelainnsf', 'delaineyhouect','delainey','ect','eea','lavoratocorpenronenron','ena','david','dave']
 
 for poi_status, from_person in [(True, poiEmailPaths), (False, notPoiEmailPaths)]:
     for path in from_person:
@@ -156,56 +149,50 @@ for poi_status, from_person in [(True, poiEmailPaths), (False, notPoiEmailPaths)
         ### use parseOutText to extract the text from the opened email
         words = parseOutText(email)
 
+        for word in replace_list:
+            words=words.replace(word, '')
+        words = words.replace('  ', ' ') # replace double spaces arising from replacement
+        
         ### append the text to word_data
         word_data.append(words)
         poi_data.append(poi_status) #mark if from poi or not from poi
             
-            
         email.close()
 
-# remove signature words that had an importance > 0.20 in tfidf vectorization
-        
-replace_list = ['ddelainnsf', 'delaineyhouect','delainey','ect','eea','lavoratocorpenronenron','ena','david','dave']
+# split data into training and testing sets
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, poi_data, test_size=0.1, random_state=42)
 
-modified_word_data = []
-
-for word in word_data:
-    for replace_word in replace_list:
-        temp_word = word.replace(replace_word, '')
-    modified_word_data.append(temp_word.replace('  ', ' '))  #replace double spaces with single space
-
-
-#split data into training set and testing set for validation
-
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(modified_word_data, poi_data, test_size=0.1, random_state=42)
-
-#run text data through tfidf vectorizer
-
+# apply tfidf vectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test).toarray()
 
-#using vectorized text data and poi_data labels, create decision tree model to predict if a person of interest
-#wrote an email
+
+# use decision tree to classify vectorized data
 
 clf = tree.DecisionTreeClassifier()
 clf.fit(features_train, labels_train)
 
+
+# create dict for storing prediction data
+
 email_prediction_data = {}
 
 for email in poiEmails:
-    email_prediction_data[email] = predict_emails(email, modified_word_data, author_data, vectorizer, clf)
+    email_prediction_data[email] = predict_emails(email, word_data, author_data, vectorizer, clf)
 
 for email in notPoiEmails:
-    email_prediction_data[email] = predict_emails(email, modified_word_data, author_data, vectorizer, clf)
+    email_prediction_data[email] = predict_emails(email, word_data, author_data, vectorizer, clf)
 
 
-# store data as pickle files to be accessed by poi_id.py
 
 pickle.dump( email_prediction_data, open("your_author_prediction_data.pkl", "w") )
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( modified_word_data, open("your_modified_word_data.pkl", "w") )
 pickle.dump( poi_data, open("your_poi_status.pkl", "w") )
 pickle.dump( author_data, open("your_author_data.pkl", "w") )
 
+with open('your_author_prediction_data.csv', 'w') as f:
+    w = csv.DictWriter(f, email_prediction_data.keys())
+    w.writeheader()
+    w.writerow(email_prediction_data)
--- poi_id.py
+++ poi_id.py
@@ -2,7 +2,10 @@
 # coding: utf-8
 
 
+
 import sys
+import csv
+import ast
 import pickle
 sys.path.append("../tools/")
 
@@ -16,15 +19,29 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 
+
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
     
-### Load the dictionary containing the text learning feature
+### Load the dictionary containing the text learning feature from csv file
+
+
+'''
 with open("your_author_prediction_data.pkl", "r") as data_file:
     email_prediction_data = pickle.load(data_file)
+'''
 
 
+with open('your_author_prediction_data.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader: # only 1 row expected
+        email_prediction_data = row
+        
+# convert csv data from form key: '[x, y]' to key: [x, y]
+for email in email_prediction_data:
+    email_prediction_data[email] = ast.literal_eval(email_prediction_data[email])
+    
 #add new feature data to data_dict
 
 for person in data_dict:
@@ -37,20 +54,20 @@ for person in data_dict:
         data_dict[person]['email_predicted_proportion'] = 'NaN'   #for those with no email address listed
 
 
-#remove outlier
+# remove outlier that is 'TOTAL' row
 
 data_dict.pop('TOTAL')
 
-#choose which features to be included in classification
+
 
 features_list = ['poi','salary', 'bonus',
                 'from_poi_to_this_person', 'from_this_person_to_poi',
                 'email_predicted_proportion']
 
-### Store to my_dataset for easy export below.
+# Store to my_dataset for easy export below.
 my_dataset = data_dict
 
-### Extract features and labels from dataset for local testing
+# Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
@@ -58,6 +75,11 @@ features_train, features_test, labels_train, labels_test =     train_test_split(
 
 clf = RandomForestClassifier(n_estimators = 100, criterion = 'entropy')
 
-#store dataset and classifier for testing by tester.py
+
+# store dataset and classifier for testing by tester.py
 
 dump_classifier_and_data(clf, my_dataset, features_list)
+
+
+
+
